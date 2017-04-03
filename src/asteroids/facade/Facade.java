822a353410784fca0555344549c5ad34971c8b3a
Rik <rik.p@live.be>
2017-04-03 17:19:10 +0200

    ik stink

--- src/asteroids/facade/Facade.java
+++ src/asteroids/facade/Facade.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import asteroids.exceptions.EntitiesOverlapException;
 import asteroids.model.Bullet;
+import asteroids.model.Entity;
 import asteroids.model.Ship;
 import asteroids.model.World;
 import asteroids.part2.CollisionListener;
@@ -77,8 +78,12 @@ public class Facade implements asteroids.part2.facade.IFacade  {
 	
 	
 	/**
-	 * Create a new ship with the given position, velocity, radius and
-	 * orientation (in radians).
+	 * 	
+	 * Create a new non-null ship with the given position, velocity, radius,
+	 * direction and mass.
+	 * 
+	 * The thruster of the new ship is initially inactive. The ship is not
+	 * located in a world.
 	 * 
 	 * @param  x
 	 * 	       The x coordinate where the new ship has to be created.
@@ -112,9 +117,9 @@ public class Facade implements asteroids.part2.facade.IFacade  {
 	}
 	
 	@Override
-	public Ship createShip(double x, double y, double xVelocity, double yVelocity, double radius, double orientation, double mass) throws ModelException {
+	public Ship createShip(double x, double y, double xVelocity, double yVelocity, double radius, double direction, double mass) throws ModelException {
 		try {
-			return new Ship(x, y, xVelocity, yVelocity, radius, orientation, mass);
+			return new Ship(x, y, xVelocity, yVelocity, radius, direction, mass);;
 		} catch (IllegalArgumentException ex) {
 			throw new ModelException(ex);
 		}
@@ -412,4 +417,296 @@ public class Facade implements asteroids.part2.facade.IFacade  {
 			throw new ModelException(ex);
 		}
 	}
+	
+
+
+		
+	/**
+	 * Terminate <code>ship</code>.
+	 */
+	public void terminateShip(Ship ship) {
+		ship.terminate();
+	}
+
+	/**
+	 * Check whether <code>ship</code> is terminated.
+	 */
+	public boolean isTerminatedShip(Ship ship) {
+		return ship.isTerminated();
+	}
+
+	/**
+	 * Return the total mass of <code>ship</code> (i.e., including bullets
+	 * loaded onto the ship).
+	 */
+	public double getShipMass(Ship ship) {
+		return ship.getMass();		
+	}
+
+	/**
+	 * Return the world of <code>ship</code>.
+	 */
+	public World getShipWorld(Ship ship) {
+		return ship.getWorld();
+	}
+
+	/**
+	 * Return whether <code>ship</code>'s thruster is active.
+	 */
+	public boolean isShipThrusterActive(Ship ship) {
+		return ship.getThrusterstate();
+	}
+
+	/**
+	 * Enables or disables <code>ship</code>'s thruster depending on the value
+	 * of the parameter <code>active</code>.
+	 */
+	public void setThrusterActive(Ship ship, boolean active) {
+		ship.tr
+	}
+
+	/**
+	 * Return the acceleration of <code>ship</code>.
+	 */
+	public double getShipAcceleration(Ship ship) throws ModelException;
+
+	/**
+	 * This method is deprecated; you should not implement nor use it.
+	 * 
+	 * 
+	 * @deprecated Since part 2 of the project. This behavior is now triggered
+	 *             through the {@link #evolve(World, double, CollisionListener)}
+	 *             method.
+	 */
+	@Override
+	@Deprecated
+	default void move(Ship ship, double dt) throws ModelException {
+		// do nothing; this behaviour is now triggered by the evolve method.
+	}
+
+	/**************
+	 * BULLET: Basic methods
+	 *************/
+
+	/**
+	 * Create a new non-null bullet with the given position, velocity and
+	 * radius,
+	 * 
+	 * The bullet is not located in a world nor loaded on a ship.
+	 */
+	public Bullet createBullet(double x, double y, double xVelocity, double yVelocity, double radius) throws ModelException;
+
+	/**
+	 * Terminate <code>bullet</code>.
+	 */
+	public void terminateBullet(Bullet bullet) throws ModelException;
+
+	/**
+	 * Check whether <code>bullet</code> is terminated.
+	 */
+	public boolean isTerminatedBullet(Bullet bullet) throws ModelException;
+
+	/**
+	 * Return the position of <code>ship</code> as an array containing the
+	 * x-coordinate, followed by the y-coordinate.
+	 */
+	public double[] getBulletPosition(Bullet bullet) throws ModelException;
+
+	/**
+	 * Return the velocity of <code>ship</code> as an array containing the
+	 * velocity along the X-axis, followed by the velocity along the Y-axis.
+	 */
+	public double[] getBulletVelocity(Bullet bullet) throws ModelException;
+
+	/**
+	 * Return the radius of <code>bullet</code>.
+	 */
+	public double getBulletRadius(Bullet bullet) throws ModelException;
+
+	/**
+	 * Return the mass of <code>bullet</code>.
+	 */
+	public double getBulletMass(Bullet bullet) throws ModelException;
+
+	/**
+	 * Return the world in which <code>bullet</code> is positioned.
+	 * 
+	 * This method must return null if a bullet is not positioned in a world, or
+	 * if it is positioned on a ship.
+	 */
+	public World getBulletWorld(Bullet bullet) throws ModelException;
+
+	/**
+	 * Return the ship in which <code>bullet</code> is positioned.
+	 * 
+	 * This method must return null if a bullet is not positioned on a ship.
+	 */
+	public Ship getBulletShip(Bullet bullet) throws ModelException;
+
+	/**
+	 * Return the ship that fired <code>bullet</code>.
+	 */
+	public Ship getBulletSource(Bullet bullet) throws ModelException;
+
+
+	/**************
+	 * WORLD: Basic methods
+	 *************/
+
+	/**
+	 * Create a new world with the given <code>width</code> and
+	 * <code>height</code>.
+	 */
+	public World createWorld(double width, double height) throws ModelException;
+
+	/**
+	 * Terminate <code>world</code>.
+	 */
+	public void terminateWorld(World world) throws ModelException;
+
+	/**
+	 * Check whether <code>world</code> is terminated.
+	 */
+	public boolean isTerminatedWorld(World world) throws ModelException;
+
+	/**
+	 * Return the size of <code>world</code> as an array containing the width,
+	 * followed by the height.
+	 */
+	public double[] getWorldSize(World world) throws ModelException;
+
+	/**
+	 * Return all ships located within <code>world</code>.
+	 */
+	public Set<? extends Ship> getWorldShips(World world) throws ModelException;
+
+	/**
+	 * Return all bullets located in <code>world</code>.
+	 */
+	public Set<? extends Bullet> getWorldBullets(World world) throws ModelException;
+
+	/**
+	 * Add <code>ship</code> to <code>world</code>.
+	 */
+	public void addShipToWorld(World world, Ship ship) throws ModelException;
+
+	/**
+	 * Remove <code>ship</code> from <code>world</code>.
+	 */
+	public void removeShipFromWorld(World world, Ship ship) throws ModelException;
+
+	/**
+	 * Add <code>bullet</code> to <code>world</code>.
+	 */
+	public void addBulletToWorld(World world, Bullet bullet) throws ModelException;
+
+	/**
+	 * Remove <code>ship</code> from <code>world</code>.
+	 */
+	public void removeBulletFromWorld(World world, Bullet bullet) throws ModelException;
+
+	
+	/**************
+	 * SHIP: Methods related to loaded bullets
+	 *************/
+
+	/**
+	 * Return the set of all bullets loaded on <code>ship</code>.
+	 * 
+	 * For students working alone, this method may return null.
+	 */
+	public Set<? extends Bullet> getBulletsOnShip(Ship ship) throws ModelException;
+
+	/**
+	 * Return the number of bullets loaded on <code>ship</code>.
+	 */
+	public int getNbBulletsOnShip(Ship ship) throws ModelException;
+
+	/**
+	 * Load <code>bullet</code> on <code>ship</code>.
+	 */
+	public void loadBulletOnShip(Ship ship, Bullet bullet) throws ModelException;
+
+	/**
+	 * Load <code>bullet</code> on <code>ship</code>.
+	 * 
+	 * For students working alone, this method must not do anything.
+	 */
+	public void loadBulletsOnShip(Ship ship, Collection<Bullet> bullets) throws ModelException;
+
+	/**
+	 * Remove <code>ship</code> from <code>ship</code>.
+	 */
+	public void removeBulletFromShip(Ship ship, Bullet bullet) throws ModelException;
+
+	/**
+	 * <code>ship</code> fires a bullet.
+	 */
+	public void fireBullet(Ship ship) throws ModelException;
+	
+
+	/******************
+	 * COLLISIONS
+	 **************/
+
+	/**
+	 * Return the shortest time in which the given entity will collide with the
+	 * boundaries of its world.
+	 */
+	public double getTimeCollisionBoundary(Object object) throws ModelException;
+
+	/**
+	 * Return the first position at which the given entity will collide with the
+	 * boundaries of its world.
+	 */
+	public double[] getPositionCollisionBoundary(Object object) throws ModelException;
+
+	/**
+	 * Return the shortest time in which the first entity will collide with the
+	 * second entity.
+	 */
+	public double getTimeCollisionEntity(Object entity1, Object entity2) throws ModelException;
+
+	/**
+	 * Return the first position at which the first entity will collide with the
+	 * second entity.
+	 */
+	public double[] getPositionCollisionEntity(Object entity1, Object entity2) throws ModelException;
+
+	/**
+	 * Return the time that must pass before a boundary collision or an entity
+	 * collision will take place in the given world. Positive Infinity is
+	 * returned if no collision will occur.
+	 */
+	public double getTimeNextCollision(World world) throws ModelException;
+
+	/**
+	 * Return the position of the first boundary collision or entity collision
+	 * that will take place in the given world. Null is returned if no collision
+	 * will occur.
+	 */
+	public double[] getPositionNextCollision(World world) throws ModelException;
+
+	/**
+	 * Advance <code>world</code> by <code>dt<code> seconds. 
+	 * 
+	 * To enable explosions within the UI, notify <code>collisionListener</code>
+	 * whenever an entity collides with a boundary or another entity during this
+	 * method. <code>collisionListener</code> may be null. If
+	 * <code>collisionListener</code> is <code>null</code>, do not call its
+	 * notify methods.
+	 */
+	public void evolve(World world, double dt, CollisionListener collisionListener) throws ModelException;
+
+	/**
+	 * Return the entity at the given <code>position</code> in the given
+	 * <code>world</code>.
+	 */
+	public Object getEntityAt(World world, double x, double y) throws ModelException;
+
+	/**
+	 * Return a set of all the entities in the given world.
+	 */
+	public Set<? extends Object> getEntities(World world) throws ModelException;
+
 }
